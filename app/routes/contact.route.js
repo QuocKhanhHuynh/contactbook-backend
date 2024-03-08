fae7c701d9fe5d298c9c4f147e665763cba4d0d8
“QuocKhanhHuynh” <khanhhuynh912@gmail.com>
2024-03-08 15:21:50 +0700

    Cai dat handler truy xuat CSDL

--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.route("/").get(contacts.findAll).post(contacts.create).delete(contacts.deletaAll);
 router.route("/favorite").get(contacts.findAllFavorite);
-router.route("/:id".get(contacts.findOne)).put(contacts.update).delete(contacts.delete);
+router.route("/:id").get(contacts.findOne).put(contacts.update).delete(contacts.delete);
 
 module.exports = router;
